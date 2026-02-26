4c7c71a4398b8f4ba761b0268573fd7b2a3fea13
Ikechukwu Onuorah <onuorah.ikechukwuvictor@gmail.com>
2026-02-26 13:40:02 +0100

    Initial Commit

---
+++ priceTrackerApp/priceTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  priceTrackerAppApp.swift
+//  priceTrackerApp
+//
+//  Created by Ikechukwu Onuorah on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct priceTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ priceTrackerAppTests/priceTrackerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  priceTrackerAppTests.swift
+//  priceTrackerAppTests
+//
+//  Created by Ikechukwu Onuorah on 26/02/2026.
+//
+
+import Testing
+@testable import priceTrackerApp
+
+struct priceTrackerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
